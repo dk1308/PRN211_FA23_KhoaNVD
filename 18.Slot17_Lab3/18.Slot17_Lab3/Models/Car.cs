a497e5a6aff9655fa560281e5c44845f5212fa9c
Dang Khoa <113330304+dk1308@users.noreply.github.com>
2023-11-07 11:32:52 +0700

    lab3

---
+++ 18.Slot17_Lab3/18.Slot17_Lab3/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _18.Slot17_Lab3.Models;
+
+public partial class Car
+{
+    public int CarId { get; set; }
+
+    public string CarName { get; set; } = null!;
+
+    public string Manufacturer { get; set; } = null!;
+
+    public double Price { get; set; }
+
+    public int ReleasedYear { get; set; }
+}
---
+++ 18.Slot17_Lab3/18.Slot17_Lab3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _18.Slot17_Lab3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
